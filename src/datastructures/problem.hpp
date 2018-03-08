85118249f075cbd8cb6eeb4a5aa07b5271029081
Matthew J.P. Walker <matthewjp.walker@mail.utoronto.ca>
2018-03-08 02:41:26 -0500

    add empty Problem and Assignments datastructures

---
+++ src/datastructures/problem.hpp
@@ -0,0 +1,16 @@
+#ifndef DATASTRUCTURES__PROBLEM_H
+#define DATASTRUCTURES__PROBLEM_H
+
+namespace simplex {
+
+class Problem {
+
+};
+
+class Assignments {
+
+};
+
+} // end namespace simplex
+
+#endif /* DATASTRUCTURES__PROBLEM_H */
