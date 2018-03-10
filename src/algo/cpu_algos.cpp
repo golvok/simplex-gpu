187d1db864eef7e701e22edfb6102e454bea9cda
Matthew J.P. Walker <matthewjp.walker@mail.utoronto.ca>
2018-03-09 19:10:45 -0500

    cpu: add iteration counting + message

--- src/algo/cpu_algos.cpp
+++ src/algo/cpu_algos.cpp
@@ -1,6 +1,7 @@
 #include "cpu_algos.hpp"
 
 #include <impl/cpu_impl.hpp>
+#include <util/logging.hpp>
 
 namespace simplex{
 namespace cpu {
@@ -9,13 +10,18 @@ boost::variant<
 	Assignments,
 	TableauErrors
 > algo_from_paper(const Problem& problem) {
+	const auto indent = dout(DL::INFO).indentWithTitle("Algorithm from the Paper (CPU)");
 	auto tableau = create_tableau(problem);
+	int iteration_num = 1;
 
 	while (true) {
+		const auto indent = dout(DL::INFO).indentWithTitle([&](auto&& s){ s << "Iteration " << iteration_num; });
+		dout(DL::DBG1) << "tableau:\n" << tableau << '\n';
+
 		const auto entering_var = find_entering_variable(tableau);
 
 		if (!entering_var) {
-			return Assignments{};
+			break;
 		}
 		
 		const auto tv_and_centering = get_theta_values_and_entering_column(tableau, *entering_var);
@@ -38,7 +44,15 @@ boost::variant<
 			tv_and_centering.entering_column,
 			entering_and_leaving
 		);
+
+		iteration_num += 1;
 	}
+
+	{const auto indent = dout(DL::INFO).indentWithTitle("Result");
+		dout(DL::INFO) << tableau;
+	}
+
+	return Assignments{};
 }
 
 } // end namespace cpu
