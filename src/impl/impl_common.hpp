fb0c3fa867f068dd64d0c982de3e763cae7aea8f
Matthew J.P. Walker <matthewjp.walker@mail.utoronto.ca>
2018-03-09 19:07:31 -0500

    add printing for VariableID

--- src/impl/impl_common.hpp
+++ src/impl/impl_common.hpp
@@ -4,11 +4,17 @@
 #include <util/id.hpp>
 
 #include <cstdint>
+#include <iostream>
 
 namespace simplex {
 
 struct VariableIDTag { static const std::int32_t DEFAULT_VALUE = INT32_MIN; };
 using VariableID = util::ID<std::int32_t, VariableIDTag>;
+
+inline std::ostream& operator<<(std::ostream& os, const VariableID& id) {
+	os << "var" << id.getValue();
+	return os;
+}
 
 struct VariablePair {
 	VariableID entering;
